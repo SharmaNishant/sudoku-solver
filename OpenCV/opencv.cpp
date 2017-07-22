74a3f3f42345df63ec734896dbb188e2b4188caf
Nishant Sharma <nishants1994@gmail.com>
2017-07-22 01:58:53 -0500

    opencv update

--- OpenCV/opencv.cpp
+++ OpenCV/opencv.cpp
@@ -4,7 +4,7 @@
 
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/contrib/contrib.hpp"
+//#include "opencv2/contrib/contrib.hpp"
 
 #include <tesseract/baseapi.h>
 #include <tesseract/strngs.h>
