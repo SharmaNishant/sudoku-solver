0656308fed4f1a7df30836e2ed240901e04eef4b
Nishant Sharma <nishants1994@gmail.com>
2017-08-05 03:18:07 -0500

    added checks

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -1,4 +1,6 @@
-$(function() {
+
+//code for reading and siplaying selected filename
+/*$(function() {
 
   // We can attach the `fileselect` event to all file inputs on the page
   $(document).on('change', ':file', function() {
@@ -22,12 +24,43 @@ $(function() {
           }
 
       });
-      
+
   });
 
 
+});*/
+
+//to display selected file name
+$("#file_select").on("change", function()
+{
+    if( (this.files[0].size/1024/1024) > 5 ){
+        alert('This file size is: ' + this.files[0].size/1024/1024 + "MB.");
+        return;
+    }
+    var value = this.value.split(/[\/\\]/).pop();
+    $("#file_select_label").val(value);
 });
 
+
+//when solveit button is clicked
+function solveSudokuFile() {
+    var input, file;
+
+    input = document.getElementById('file_select');
+    if (!input) {
+        alert("Um, couldn't find the fileinput element.");
+    }
+    else if (!input.files) {
+        alert("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+        alert("Please select a file before clicking 'Solve it!'");
+    }
+    else {
+        file = input.files[0];
+        alert("Successful");
+    }
+}
 
 
 $(function(){ 
@@ -40,17 +73,25 @@ $(document).ready(function () {
 
 
 function generateSudokuGrid(data) {
-    return $('<table>').append(multiPush(9, function () {
-        return $('<tr>').append(multiPush(9, function () {
-            return $('<td class="cell"><input type="number" min="0" max="9"></div>');
+    return $('<table>').append( rowPush(9, function () {
+        return $('<tr>').append( colPush(9, arguments[0], function () {
+            return $('<td class="cell"><input type="number" min="0" max="9" id="input'+ arguments[1] + arguments[0] + '"></div>');
         }));
     })).addClass('sudoku');
 }
 
-function multiPush(count, func, scope) {
+function rowPush(count, func, scope) {
     var arr = [];
     for (var i = 0; i < count; i++) {
         arr.push(func.call(scope, i));
+    }
+    return arr;
+}
+
+function colPush(count, rowNum, func, scope) {
+    var arr = [];
+    for (var i = 0; i < count; i++) {
+        arr.push(func.call(scope, i, arguments[1]));
     }
     return arr;
 }
